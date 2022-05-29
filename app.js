c2dbf09d234b14604eaf80a13a30d3e219d854ae
andrevdal <andreas.drevdal@gmail.com>
2022-05-29 22:49:41 +0200

    basic css, navbar, footer and title

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/heroku", (req, res) => {
+  res.sendFile(__dirname + "/pages/heroku.html");
+});
+
+app.get("/github", (req, res) => {
+  res.sendFile(__dirname + "/pages/github.html");
+});
+
+app.get("/sammenligning", (req, res) => {
+  res.sendFile(__dirname + "/pages/sammenligning.html");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
